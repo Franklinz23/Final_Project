9d5a1ccae62adad97990a41b45201496b1d66d0a
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-26 15:27:16 -0700

    added students model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526220632) do
+ActiveRecord::Schema.define(version: 20160526222432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string   "f_name"
+    t.string   "l_name"
+    t.string   "grade"
+    t.string   "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
