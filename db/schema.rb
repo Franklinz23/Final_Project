65fdbfe87bc80b62b264c3d4cb676f2d9afdd17c
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-06-01 13:00:23 -0700

    made it so ONLY teachers can post events o events on calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531194141) do
+ActiveRecord::Schema.define(version: 20160531215312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "meetings", force: :cascade do |t|
     t.string   "name"
     t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "students", force: :cascade do |t|
